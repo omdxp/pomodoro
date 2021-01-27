9e93ab10838568dcf39e4600e9c1f70971d971e3
Omar-Belghaouti <bel_omar18@yahoo.com>
2021-01-27 09:30:45 +0100

    Testing new custom components

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import {Text, View} from 'react-native';
+import Button from '../components/button';
+import SizedBox from '../components/sized-box';
+import {textStyles} from '../styles';
 
 export default function HomeScreen() {
   return (
     <View>
-      <Text>HomeScreen</Text>
+      <Text style={textStyles.title}>Hello...</Text>
+      <SizedBox vertical={10} />
+      <Button title={'Start'} onPress={() => alert('What?')} />
     </View>
   );
 }
