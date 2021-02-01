239a3dbece572e922c772b0aeb3e7fb68c1d498d
Omar-Belghaouti <bel_omar18@yahoo.com>
2021-02-01 14:04:22 +0100

    Adding SizedBox from npm

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import {Text, View} from 'react-native';
 import Button from '../components/button';
-import SizedBox from '../components/sized-box';
 import {textStyles} from '../styles';
+import {SizedBox} from 'sizedbox';
 
 export default function HomeScreen() {
   return (
     <View>
       <Text style={textStyles.title}>Hello...</Text>
-      <SizedBox vertical={10} />
-      <Button title={'Start'} onPress={() => alert('What?')} />
+      <SizedBox vertical={50} />
+      {/* <Button title={'Start'} onPress={() => alert('What?')} /> */}
+      <Text>Hi</Text>
+      <View style={{flexDirection: 'row'}}>
+        <Text>Omar</Text>
+        <SizedBox horizontal={200} />
+        <Text>Yasser</Text>
+      </View>
     </View>
   );
 }
