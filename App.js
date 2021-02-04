ab3dd6dcad82383543e18e71ff1351ce9fda6368
Omar-Belghaouti <bel_omar18@yahoo.com>
2021-02-04 22:02:17 +0100

    providing redux to app

--- App.js
+++ App.js
@@ -5,11 +5,17 @@ import {NavigationContainer} from '@react-navigation/native';
 import Auth from './app/screens/auth/Navigation';
 import Main from './app/screens/Navigation';
 
+// import redux
+import {Provider} from 'react-redux';
+import {store} from './app/redux/store';
+
 export default function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
-    <NavigationContainer>
-      {isLoggedIn ? <Main /> : <Auth />}
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        {isLoggedIn ? <Main /> : <Auth />}
+      </NavigationContainer>
+    </Provider>
   );
 }
