53a8ff28bce89f0fd17f7e1a4ac90e8350d328a0
Omar-Belghaouti <bel_omar18@yahoo.com>
2021-02-12 18:32:22 +0100

    Remove Test screen from drawer nav ☄

--- app/screens/Navigation.js
+++ app/screens/Navigation.js
@@ -2,7 +2,6 @@ import React from 'react';
 // import drawer navigation
 import {createDrawerNavigator} from '@react-navigation/drawer';
 import Home from './home/ui/HomeUI';
-import Test from './test/ui/TestUI';
 
 // create drawer menu
 const Drawer = createDrawerNavigator();
@@ -11,7 +10,6 @@ export default function Navigation() {
   return (
     <Drawer.Navigator>
       <Drawer.Screen name="Home" component={Home} />
-      <Drawer.Screen name="Test" component={Test} />
     </Drawer.Navigator>
   );
 }
