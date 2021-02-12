6c1b6edd8bd7851950575db8fc2b091386d98d56
Omar-Belghaouti <bel_omar18@yahoo.com>
2021-02-12 19:08:25 +0100

    Update settings ui 🧥

--- app/screens/settings/ui/SettingsUI.js
+++ app/screens/settings/ui/SettingsUI.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import {screenStyles, textStyles} from '../../../styles';
 
 export default function SettingsUI() {
   return (
-    <View>
-      <Text>SettingsUI</Text>
+    <View style={screenStyles.container}>
+      <Text style={textStyles.title}>Settings</Text>
     </View>
   );
 }
