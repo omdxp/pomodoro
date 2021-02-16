22e706776198270e6f867795fa3b7e8ce00825f0
Omar-Belghaouti <bel_omar18@yahoo.com>
2021-02-16 18:01:51 +0100

    add history screen 🕶

---
+++ app/screens/history/ui/historyUI.js
@@ -0,0 +1,16 @@
+
+// import react native
+import React from 'react';
+import {Text, View} from 'react-native';
+
+// import history functions
+import {} from '../functions';
+
+// export history UI
+export default function historyUI() {
+  return (
+    <View>
+      <Text>history UI created!</Text>
+    </View>
+  );
+}
